c8bf840f2399becae6fe7b4119db4e7572baef18
rveras1 <raul.veras@disneystreaming.com>
2026-01-13 04:19:22 -0500

    adding input logic to firstLetterUpperCase.js module

--- firstLetterUpperCase.js
+++ firstLetterUpperCase.js
@@ -1,22 +1,27 @@
-
-
+const readline = require('readline')
 
 function firstLetterUpperCase(str) {
     let newStr = str.toString().split(' ');
-    console.log(newStr);
     let firstLetter = newStr[0].toUpperCase();
 
     const upperStr = newStr.map((index) => {
         let strArray = index.toString().split('');
-        console.log(strArray)
         let charUpper = strArray[0].toUpperCase();
         strArray.shift()
         strArray.unshift(charUpper)
         return strArray.join('');
     })
-    return upperStr.join(' ')
+    return upperStr.join(' ');
 
 };
-console.log(firstLetterUpperCase('advanced micro devices inc.'))
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter a phrase in all lower-case: ', (answer) => {
+    console.log(firstLetterUpperCase(`${answer}`));
+    rl.close();
+});
 
